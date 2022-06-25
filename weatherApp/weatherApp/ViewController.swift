a1b3c1b99eeb3ca45b3ff84eace51604f2161071
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-06-25 14:34:13 +0300

    blur view transformed from code to storyboard and add constraints

--- weatherApp/weatherApp/ViewController.swift
+++ weatherApp/weatherApp/ViewController.swift
@@ -9,27 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var topConstraint = NSLayoutConstraint()
+    var bottomConstraint = NSLayoutConstraint()
+    var trailingConstraint = NSLayoutConstraint()
+    var leadingConstraint = NSLayoutConstraint()
     
     @IBOutlet weak var summaryWeatherInfo: UILabel!
     @IBOutlet weak var currentWeatherImg: UIImageView!
     @IBOutlet weak var cityNameLable: UILabel!
     @IBOutlet weak var textWeatherDiscription: UILabel!
     @IBOutlet weak var sunIndexLable: UILabel!
-    @IBOutlet weak var viewForBackgroundBlurePic: UIView!
     @IBOutlet weak var imageViewForBackgroundPic: UIImageView!
     
     var weatherManager = WeatherManager()
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setbackgroundPic()
+        imageViewForBackgroundPic.image = UIImage(named: "backgroundPic")
         weatherManager.urlRequest(city: "Dublin")
         weatherManager.completion = { [weak  self] currentWeather in
             guard let self = self else { return }
             self.update(weather: currentWeather)
         }
     } //конец вью дид лод
-    //    MARK: - update weather data with necessary parametrs 
+    //    MARK: - update weather data with necessary parametrs
     func update(weather: CurrentAndForecastWeather) {
         guard let icon = weather.current?.weather?.first?.icon else { return }
         let iconURL = URL(string: "https://openweathermap.org/img/wn/\(icon)@2x.png")
@@ -54,19 +58,5 @@ class ViewController: UIViewController {
             self.textWeatherDiscription.text = "Now \(description)"
             self.sunIndexLable.text = "Sun Index is \(Double(sunIndex))"
         }
-    }
-//    MARK: - set background image with blure effect
-    func setbackgroundPic() {
-        imageViewForBackgroundPic.image = UIImage(named: "backgroundPic")
-        
-//            let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
-//                backgroundImage.image = UIImage(named: "backgroundPic")
-//            backgroundImage.contentMode = .bottom
-        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.light)
-            let blurEffectView = UIVisualEffectView(effect: blurEffect)
-            blurEffectView.frame = viewForBackgroundBlurePic.bounds
-        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-            viewForBackgroundBlurePic.addSubview(blurEffectView)
-//                self.viewForBackgroundBlurePic.insertSubview(backgroundImage, at: 0)
     }
 }//конец класса
