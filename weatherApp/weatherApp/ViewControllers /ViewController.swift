1206628d9f8d23be52532b2b47846e02710b4da6
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-07-07 11:30:48 +0300

    add forecast by hour check

--- weatherApp/weatherApp/ViewControllers /ViewController.swift
+++ weatherApp/weatherApp/ViewControllers /ViewController.swift
@@ -43,6 +43,9 @@ class ViewController: UIViewController {
             self.hourlyWeather = weatherData.hourly
             self.update()
             self.realmManager.reciveData(data: weatherData)
+            guard let weather = self.hourlyWeather else { return }
+            self.weatherForecast(hourlyWeather: weather)
+            self.removeAllNotification()
             
         }
     } //конец вью дид лод
@@ -97,10 +100,41 @@ class ViewController: UIViewController {
         }
     }
     
+    func getDateComponentsFrom(date: Int) -> DateComponents {
+        let calendar = Calendar.current
+        let newDate = Date(timeIntervalSince1970: TimeInterval(date))
+        var newDateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: newDate)
+        guard let minutes = newDateComponents.minute else { fatalError() }
+        newDateComponents.minute = minutes - 30
+        return newDateComponents
+    }
+    
+    func weatherForecast(hourlyWeather: [HourlyWeatherData]) {
+        var index = 0
+        for hour in hourlyWeather {
+            guard let id = hour.weather?.first?.id, let time = hour.dt else { return }
+            if index == 0 || index > 3 {
+                switch id {
+                case 200...232:
+                    setLocalNotification(body: "Thunderstorm is coming", title: "Attantion", dateComponents: getDateComponentsFrom(date: time))
+                    index = 1
+                case 500...531:
+                    setLocalNotification(body: "Rain is coming", title: "Attantion", dateComponents: getDateComponentsFrom(date: time))
+                    index = 1
+                case 600...622:
+                    setLocalNotification(body: "Snow is coming", title: "Attantion", dateComponents: getDateComponentsFrom(date: time))
+                    index = 1
+                default: break
+                }
+            }
+            index += 1
+        }
+    }
+    
     func removeAllNotification() {
         notificationCenter.removeAllPendingNotificationRequests()
     }
-    //    MARK: - end local notification 
+    //    MARK: - end local notification
 } //конец класса
 
 
