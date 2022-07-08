2e6c7fd5a4c182901c6109a502dcf625425f3b4e
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-07-08 11:01:50 +0300

    add deleteRealmIfMigrationNeeded

--- weatherApp/weatherApp/Manager/RealmDataBaseManager .swift
+++ weatherApp/weatherApp/Manager/RealmDataBaseManager .swift
@@ -19,7 +19,8 @@ class RealmManager: RealmDataBaseProtocol {
     var realm: Realm!
     
     init() {
-        let configuration = Realm.Configuration()
+        var configuration = Realm.Configuration()
+        configuration.deleteRealmIfMigrationNeeded = true
         do {
             realm = try Realm(configuration: configuration)
         } catch {
