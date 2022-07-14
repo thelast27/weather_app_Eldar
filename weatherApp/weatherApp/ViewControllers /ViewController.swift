c77d60f53e1f359b70b71d309b52151fd24d0c6b
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-07-14 14:32:24 +0300

    dix date bug in table view with forecast

--- weatherApp/weatherApp/ViewControllers /ViewController.swift
+++ weatherApp/weatherApp/ViewControllers /ViewController.swift
@@ -153,6 +153,6 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Weather forecast for 10 days"
+        return "Weather forecast for 7 days"
     }
 }
